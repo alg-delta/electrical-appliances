39b49b7b056aeac7db2102f1b4934fce26540224
LarysaHliieva <lora.glievaya@gmail.com>
2026-06-05 21:33:17 +0300

    fix db init

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
-
+with app.app_context():
+    db.create_all()
 
 # --- МАРШРУТИ (ROUTES) ---
 
